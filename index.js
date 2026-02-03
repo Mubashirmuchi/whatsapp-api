8a619973e60defede42e89d1e780ea8bd5b8f59b
Mubashir <mubashirmuchi75@gmail.com>
2026-02-03 16:36:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,221 @@
+require("dotenv").config();
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+
+// async function sendTemplateMessage() {
+//   const response = await axios({
+//     url: `https://graph.facebook.com/v22.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+
+//     method: "post",
+//     headers: {
+//       Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+//       "Content-Type": "application/json",
+//     },
+//     data: JSON.stringify({
+//       messaging_product: "whatsapp",
+//       to: process.env.SEND_TO,
+//       type: "template",
+//       template: {
+//         name: "gym_fee",
+//         language: {
+//           code: "en_US",
+//         },
+//         // components: [
+//         //   {
+//         //     type: "header",
+//         //     parameters: [
+//         //       {
+//         //         type: "text",
+//         //         text: "John Doe",
+//         //       },
+//         //     ],
+//         //   },
+//         //   {
+//         //     type: "body",
+//         //     parameters: [
+//         //       {
+//         //         type: "text",
+//         //         text: "50",
+//         //       },
+//         //     ],
+//         //   },
+//         // ],
+//       },
+//     }),
+//   });
+
+//   console.log(response.data);
+// }
+
+// async function sendTemplateMessage() {
+//   const response = await axios({
+//     url: `https://graph.facebook.com/v22.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+//     method: "post",
+//     headers: {
+//       Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+//       "Content-Type": "application/json",
+//     },
+//     data: JSON.stringify({
+//       messaging_product: "whatsapp",
+//       to: process.env.SEND_TO,
+//       type: "template",
+//       template: {
+//         name: "gym_fee",
+//         language: {
+//           code: "en_US",
+//         },
+//         components: [
+//           // Move inside template object
+//           {
+//             type: "header",
+//             parameters: [
+//               {
+//                 type: "text",
+//                 text: "John Doe",
+//               },
+//             ],
+//           },
+//           {
+//             type: "body",
+//             parameters: [
+//               {
+//                 type: "text",
+//                 text: "50", // Parameter 1
+//               },
+//               {
+//                 type: "text",
+//                 text: "January", // Parameter 2
+//               },
+//               {
+//                 type: "text",
+//                 text: "2024", // Parameter 3
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     }),
+//   });
+
+//   console.log(response.data);
+// }
+
+async function sendTemplateMessage() {
+  const response = await axios({
+    url: `https://graph.facebook.com/v22.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      messaging_product: "whatsapp",
+      to: process.env.SEND_TO,
+      type: "template",
+      template: {
+        name: "gym_fee",
+        language: {
+          code: "en_US",
+        },
+        components: [
+          {
+            type: "body",
+            parameters: [
+              {
+                type: "text",
+                text: "John", // {{1}} - Member name
+              },
+              {
+                type: "text",
+                text: "Broadband", // {{2}} - Bill type/service
+              },
+              {
+                type: "text",
+                text: "$50", // {{3}} - Amount
+              },
+              {
+                type: "text",
+                text: "2025-12-31", // {{4}} - Due date
+              },
+            ],
+          },
+        ],
+      },
+    }),
+  });
+
+  console.log(response.data);
+}
+
+async function sendTextMessage() {
+  const response = await axios({
+    url: "https://graph.facebook.com/v20.0/phone_number_id/messages",
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      messaging_product: "whatsapp",
+      to: "phone_number",
+      type: "text",
+      text: {
+        body: "This is a text message",
+      },
+    }),
+  });
+
+  console.log(response.data);
+}
+
+async function sendMediaMessage() {
+  const response = await axios({
+    url: "https://graph.facebook.com/v20.0/phone_number_id/messages",
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      messaging_product: "whatsapp",
+      to: "phone_number",
+      type: "image",
+      image: {
+        //link: 'https://dummyimage.com/600x400/000/fff.png&text=manfra.io',
+        id: "512126264622813",
+        caption: "This is a media message",
+      },
+    }),
+  });
+
+  console.log(response.data);
+}
+
+async function uploadImage() {
+  const data = new FormData();
+  data.append("messaging_product", "whatsapp");
+  data.append("file", fs.createReadStream(process.cwd() + "/logo.png"), {
+    contentType: "image/png",
+  });
+  data.append("type", "image/png");
+
+  const response = await axios({
+    url: "https://graph.facebook.com/v20.0/phone_number_id/media",
+    method: "post",
+    headers: {
+      Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+    },
+    data: data,
+  });
+
+  console.log(response.data);
+}
+
+sendTemplateMessage();
+
+// sendTextMessage()
+
+// sendMediaMessage()
+
+// uploadImage()
